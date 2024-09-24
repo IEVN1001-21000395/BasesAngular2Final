956d3ab17aff05eacfc5366ef771701a66dda96c
Cristian Santiago Serrano Ferro <cserranoferro171@gmail.com>
2024-09-23 20:38:27 -0600

    23/09/2024 filtros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductoFilterPipe } from './product/producto-filter.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductoFilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductos } from '../productos';
 
 @Component({
   selector: 'app-product-list',
@@ -10,40 +11,41 @@ export class ProductListComponent {
   imageWidth:number=50;
   imageMargin:number=2;
   muestraImg:boolean=true;
+  listFilter:string="";
   mostrarImagen():void{
     this.muestraImg=!this.muestraImg
   }
 
-  productos:any[]=[
+  productos:IProductos[]=[
     {
       "ProductoId":1,
-      "Modelo":"Sentra",
+      "Modelo":"Versa",
       "Descripcion": "4 Puertas",
       "Year":"marzo 2 2020",
-      "Precio":"2000",
-      "Color":"Azul",
-      "Marca":"AUDI",
-      "ImagenUrl": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.expansion.com%2Fempresas%2Fmotor%2F2023%2F01%2F02%2F63b2c5afe5fdeaa86d8b456d.html&psig=AOvVaw0dLp87nnQRTEIbdCH2Az9R&ust=1726886349108000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLjjqI2_0IgDFQAAAAAdAAAAABAE"
+      "Precio":2000,
+      "Color":"Rojo",
+      "Marca":"NISSAN",
+      "ImagenUrl": "https://th.bing.com/th/id/R.b60ff7bb9e3b1cfbaa7b2498c8558431?rik=YJ4JuVqbsY7M%2bQ&pid=ImgRaw&r=0"
     },
     {
       "ProductoId":2,
-      "Modelo":"Rio",
-      "Descripcion": "4 Puertas",
+      "Modelo":"A5",
+      "Descripcion": "2 Puertas",
       "Year":"marzo 2 2024",
-      "Precio":"4000",
-      "Color":"Blanco",
+      "Precio":4000,
+      "Color":"Azul",
       "Marca":"AUDI",
-      "ImagenUrl": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.autobild.es%2Fnoticias%2Flamborghini-fin-ha-confirmado-tipo-coche-tanto-tiempo-llevabamos-esperando-1283840&psig=AOvVaw0dLp87nnQRTEIbdCH2Az9R&ust=1726886349108000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLjjqI2_0IgDFQAAAAAdAAAAABAJ "
+      "ImagenUrl": "https://www.gannett-cdn.com/presto/2018/09/04/PDTN/5a9a8046-21df-449e-b5f6-039d76cfaa5e-_29R0298.jpg?crop=5183,2915,x1,y411&width=3200&height=1680&fit=bounds"
     },
     {
       "ProductoId":3,
       "Modelo":"Aveo",
       "Descripcion": "4 Puertas",
       "Year":"septiempre 19 2025",
-      "Precio":"4500",
-      "Color":"negro",
-      "Marca":"AUDI",
-      "ImagenUrl": ""
+      "Precio":4500,
+      "Color":"perla azulado",
+      "Marca":"CHEVROLET",
+      "ImagenUrl": "https://th.bing.com/th/id/OIP.DxV8yj1_8k9QNCkJrhk99gHaE9?rs=1&pid=ImgDetMain"
     },
   ]
 }
